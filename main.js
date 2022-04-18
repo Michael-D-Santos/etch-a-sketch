2dd6b6344cd2c45e8286da84caebb2c9fb219234
TSC7 <michaelsantos1337@gmail.com>
2022-04-17 22:22:28 -0400

    Added comments

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const container = document.querySelector(".container");
-const button = document.querySelector(".button")
-let pixel = document.querySelector(".pixel");
-let input = prompt("Test");
+//Creates a square grid that changes the color of each pixel when hovered over.
 
 function createGrid() {
   for (i = 0; i < input ** 2; i++) {
@@ -15,9 +12,25 @@ function createGrid() {
   }
 }
 
-createGrid();
+//Declares variables corresponding to HTML classes
+
+const container = document.querySelector(".container");
+const button = document.querySelector(".button")
+
+//Determines the size of the square grid
+
+let input = prompt("How many squares do you want on each side?");
+
+//Determines the amount of columns for the square grid
 
 container.style.gridTemplateColumns=`repeat(${input}, 1fr)`;
+
+//Displays the square grid
+
+createGrid();
+
+//When the "clear grid" button is clicked; clear the grid and create a new one
+//in its place.
 
 button.addEventListener("click", () => {
   container.textContent = "";
