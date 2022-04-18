64f8a7c70df21e1a76784488d386a2800464560a
TSC7 <michaelsantos1337@gmail.com>
2022-04-17 22:10:58 -0400

    Added a clear button that successfully clears grid and returns a new one in its place.

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button")
 let pixel = document.querySelector(".pixel");
 let input = prompt("Test");
 
-for (i = 0; i < input ** 2; i++) {
-  let pixel = document.createElement("div");
-  pixel.classList.add("pixel");
-  pixel.textContent = "";
-  container.appendChild(pixel);
-  pixel.addEventListener("mouseover", () => {
-    pixel.classList.toggle("color");
-  });
+function createGrid() {
+  for (i = 0; i < input ** 2; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.textContent = "";
+    container.appendChild(pixel);
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.toggle("color");
+    });
+  }
 }
 
+createGrid();
+
 container.style.gridTemplateColumns=`repeat(${input}, 1fr)`;
+
+button.addEventListener("click", () => {
+  container.textContent = "";
+  createGrid();
+});
