a34c450af4faa4ece72dabf4986e45153937b38d
TSC7 <michaelsantos1337@gmail.com>
2022-04-17 20:58:03 -0400

    Figured out a way to change the color for each div hovered over

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const container = document.querySelector(".container");
+let pixel = document.querySelector(".pixel");
+let input = prompt("Test");
 
-for (i = 0; i < 256; i++) {
+for (i = 0; i < input ** 2; i++) {
   let pixel = document.createElement("div");
   pixel.classList.add("pixel");
   pixel.textContent = "";
   container.appendChild(pixel);
+  pixel.addEventListener("mouseover", () => {
+    pixel.classList.toggle("color");
+  });
 }
 
-container.style.gridTemplateColumns=`repeat(16, 1fr)`;
+container.style.gridTemplateColumns=`repeat(${input}, 1fr)`;
