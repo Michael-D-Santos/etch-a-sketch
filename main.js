c61a65d940a5d438a5c0ea2084b518712f4604b8
TSC7 <michaelsantos1337@gmail.com>
2022-04-17 23:35:13 -0400

    Fixed comments

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-//Creates a square grid that changes the color of each pixel when hovered over.
+//A function that creates a square grid. Its algorithm is as follows:
+//• Get input from user to determine grid size and amount of columns
+//• If the input is not a whole number from 1-100, reprompt
+//• Then, create the square itself using a for loop, and change the color of
+//each pixel when they are hovered over.
 
 function createGrid() {
+  let input = prompt("How many squares do you want on each side?");
+  while (/^(100|[1-9][0-9]?)$/.test(input) == false) {
+    input = prompt("How many squares do you want on each side?");
+  }
   for (i = 0; i < input ** 2; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
@@ -10,6 +18,7 @@ function createGrid() {
       pixel.classList.toggle("color");
     });
   }
+  container.style.gridTemplateColumns=`repeat(${input}, 1fr)`;
 }
 
 //Declares variables corresponding to HTML classes
@@ -17,20 +26,11 @@ function createGrid() {
 const container = document.querySelector(".container");
 const button = document.querySelector(".button")
 
-//Determines the size of the square grid
-
-let input = prompt("How many squares do you want on each side?");
-
-//Determines the amount of columns for the square grid
-
-container.style.gridTemplateColumns=`repeat(${input}, 1fr)`;
-
 //Displays the square grid
 
 createGrid();
 
-//When the "clear grid" button is clicked; clear the grid and create a new one
-//in its place.
+//When the "clear grid" button is clicked; clear the grid and create a new one.
 
 button.addEventListener("click", () => {
   container.textContent = "";
